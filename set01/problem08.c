6b6a856d7cb510069c62560cb68157bc8144ffd0
Sanskriti75 <sanskritis.bsc23@rvu.edu.in>
2024-02-17 11:18:39 +0530

    test

---
+++ set01/problem08.c
@@ -0,0 +1,10 @@
+//Write a C program to find sum of n different numbers entered by the user.
+#include<stdio.h>
+int input_array_size();
+int input_array(int n,int a[n]);
+int sum_n_array(int n,int a[n],int sum);
+void output(int n,int sum);
+int main()
+{
+    
+}
