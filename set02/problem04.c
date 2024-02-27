c14e97fec1ca9df44b6ca2c694e5ac29e64527ec
Sanskriti75 <142883280+Sanskriti75@users.noreply.github.com>
2024-02-27 14:44:07 +0530

    committed 2/27/2024, 2:44:07 PM

---
+++ set02/problem04.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int input_array_size()
+{
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    return n;
+}void input_array(int n,int a[n])
+{
+    printf("Enter the elements of the array:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d" , &a[i]);
+    }
+}
+int sum_composite_numbers(int n,int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>1)
+        {
+            for(int j=2;j<=a[i]/2;j++)
+            {
+                if(a[i]%j==0)
+                {
+                    sum+=a[i];
+                    break;
+                }
+            }
+        }
+    }
+    return sum;
+}
+void output(int sum)
+{
+    printf("the sum of composite number is %d",sum);
+}
+int main()
+{
+    int n,a[100],sum;
+    n=input_array_size();
+    input_array(n,a);
+    sum=sum_composite_numbers(n,a);
+    output(sum);
+
+}
